5969034669d5a95730213e72e0806e8f03daebff
Diki Rahmad Sandi <95979203+wawaiguntang@users.noreply.github.com>
2022-11-14 18:12:18 +0700

    installer app

---
+++ installer_app.php
@@ -0,0 +1,51 @@
+<?php
+
+function custom_copy($src, $dst)
+{
+
+    $dir = opendir($src);
+
+    @mkdir($dst);
+
+    while ($file = readdir($dir)) {
+
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                custom_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                copy($src . '/' . $file, $dst . '/' . $file);
+            }
+        }
+    }
+
+    closedir($dir);
+}
+
+$dir = __DIR__;
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+switch ($request_method) {
+    case 'POST':
+        $clientID = $_GET["clientID"];
+        $app = $_GET["app"];
+        $src = $dir . '/alin-'.$app;
+        $dst = $dir . '/alin-'.$app.'-' . $clientID;
+        $copy = custom_copy($src, $dst);
+        if ($copy) {
+            $response = array(
+                'status' => TRUE
+            );
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }else{
+            $response = array(
+                'status' => FALSE
+            );
+            header('Content-Type: application/json');
+            echo json_encode($response);
+        }
+        break;
+    default:
+        header("HTTP/1.0 405 Method Not Allowed");
+        break;
+}
